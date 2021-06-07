14b81a09ef7207166ce1f627ddd94bd23c61017c
Tyler Harpool <tyler_harpool@arwd.uscourts.gov>
2021-06-07 02:06:03 -0500

    Updated /blog route

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ async fn main() -> std::io::Result<()> {
             .service(hello)
             .service(echo)
             .route("/hey", web::get().to(manual_hello))
+            .route("/blog", web::get().to(blog))
     })
     .bind("127.0.0.1:8080")?
     .run()
@@ -16,7 +17,7 @@ use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
 #[get("/")]
 async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello Lilly")
+    HttpResponse::Ok().body("Hello World!")
 }
 
 #[post("/echo")]
@@ -26,4 +27,8 @@ async fn echo(req_body: String) -> impl Responder {
 
 async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
+}
+
+async fn blog() -> impl Responder {
+    HttpResponse::Ok().body("Welcome to my Blog")
 }
